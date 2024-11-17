e218dec455607eeff500162df5b757bc6a92e5ee
Volkov Ilya <ilia.volkov@grphn.ru>
2024-11-17 17:41:37 +0300

    add migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	gocrud "go-crud"
 	"go-crud/pkg/handler"
 	"go-crud/pkg/repository"
@@ -9,12 +11,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(gocrud.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal("Error starting server:", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
